862c35396d699bacdc46547e6448a5ce3ac9e982
Mehul-vaishnav01 <vaishnavmehul12@gmail.com>
2026-09-05 21:33:29 +0530

    Doctor Controller created

---
+++ Backend/src/controller/doctor.controller.js
@@ -0,0 +1,40 @@
+import doctorModel from "../models/doctor.model";
+
+
+async function changeAvalablity(req,res) {
+    try {
+        const {docId}=req.body
+
+        const docData=await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId,{avalible:!docData.avalible})
+
+        res.status(201).json({
+            message:"Avaliblity Changed"
+        })
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+    
+}
+
+
+async function doctorList(req,res) {
+    try {
+        const doctors=await doctorModel.find({}).select(['-password','-email'])
+        res.status(200).json({
+            doctors
+        })
+        
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message: error.message
+        });
+    }    
+}
+export {changeAvalablity,doctorList}
--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -3,11 +3,13 @@ import { addDoctor } from "../controller/admin.controller.js";
 import upload from "../middleware/multer.js";
 import { loginAdmin } from "../controller/admin.controller.js";
 import authAdmin from "../middleware/auth.admin.js";
+import { changeAvalablity } from "../controller/doctor.controller.js";
 
 
 const adminRouter=express.Router();
 
 adminRouter.post('/add-Doctor',authAdmin,upload.single('image'),addDoctor)
 adminRouter.post('/login',loginAdmin)
+adminRouter.post('/chnage-avaliblity',authAdmin,changeAvalablity)
 
 export default adminRouter;
